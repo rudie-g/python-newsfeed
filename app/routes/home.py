afaf04f7bd36200e4a41f3338b04cb114db51e93
Rudie G <rauoldm@gmail.com>
2021-09-24 10:35:55 -0500

    added function to close database

--- app/routes/home.py
+++ app/routes/home.py
@@ -9,7 +9,10 @@ def index():
   db = get_db()
   posts = db.query(Post).order_by(Post.created_at.desc()).all()
 
-  return render_template('homepage.html')
+  return render_template(
+  'homepage.html',
+  posts=posts
+)
 @bp.route('/login')
 def login():
   return render_template('login.html')
